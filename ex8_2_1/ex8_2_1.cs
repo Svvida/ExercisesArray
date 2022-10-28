78ce00a0fbb4d21b3dcc41d999da88c52927dd5e
Swida <ksvvida@gmail.com>
2022-10-28 13:27:56 +0200

    Read 'n' array elements, then find and print the biggest one

--- ex8_2_1/ex8_2_1.cs
+++ ex8_2_1/ex8_2_1.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+namespace exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Input number of array elements:");
+            int numberOfUserInputs = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[numberOfUserInputs];
+            int i;
+
+            Console.WriteLine("Input " + numberOfUserInputs + " elements in the array :");
+            for (i = 0; i < numberOfUserInputs; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Elements in array are: ");
+            for (i = 0; i < numberOfUserInputs; i++)
+            {
+                Console.Write("{0}  ", array[i]);
+            }
+            Console.Write("\n");
+            int maxValue = array.Max();
+            System.Console.WriteLine("The biggest number is: " + maxValue);
+        }
+    }
+}
