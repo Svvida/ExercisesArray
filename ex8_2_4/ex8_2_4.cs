df4293813de340827086c4f36677d7ff83fbf8b4
Swida <ksvvida@gmail.com>
2022-10-31 14:48:46 +0100

    Input 'n' array elements, find and print the smallest one and count how many times it occur in array

---
+++ ex8_2_4/ex8_2_4.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            System.Console.WriteLine("Input number of array elements:");
+            int numberOfUserInputs = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[numberOfUserInputs];
+            int i;
+
+            Console.WriteLine("Input " + numberOfUserInputs + " elements in the array :");
+
+            for (i = 0; i < numberOfUserInputs; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Elements in array are: ");
+            for (i = 0; i < numberOfUserInputs; i++)
+            {
+                Console.Write("{0}  ", array[i]);
+            }
+            System.Console.WriteLine("");
+
+            Console.WriteLine("***************************************");
+            int minValue = array.Min();
+            System.Console.WriteLine("The smallest number is: " + minValue);
+
+            Console.WriteLine("***************************************");
+            int numberOfMinValues = 0;
+            foreach (int g in array)
+            {
+                if (g == minValue)
+                    numberOfMinValues++;
+            }
+            System.Console.WriteLine("Number of the smallest value is = {0}", numberOfMinValues);
+        }
+    }
+}
