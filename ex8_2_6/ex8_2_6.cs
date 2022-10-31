a6ff7299b667f589c31677860c88805321190ddd
Swida <ksvvida@gmail.com>
2022-10-31 15:06:13 +0100

    Read and print second smallest number in array

---
+++ ex8_2_6/ex8_2_6.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("Input number of array elements:");
+            int numberOfUserInputs = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[numberOfUserInputs];
+            int i;
+
+            Console.WriteLine("Input " + numberOfUserInputs + " elements in the array :");
+            for (i = 0; i < numberOfUserInputs; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Elements in array are: ");
+            for (i = 0; i < numberOfUserInputs; i++)
+            {
+                Console.Write("{0}  ", array[i]);
+            }
+            Console.Write("\n");
+            int smallest = int.MaxValue;
+            int second = int.MaxValue;
+            foreach (int j in array)
+            {
+                if (j < smallest)
+                {
+                    second = smallest;
+                    smallest = j;
+                }
+                else if (j < second)
+                    second = j;
+            }
+
+            System.Console.WriteLine("Second smallest number is " + second);
+        }
+    }
+}
