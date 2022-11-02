807158e9fc9df10de32d985f5e93ee6ace285f28
Swida <ksvvida@gmail.com>
2022-11-02 09:38:54 +0100

    Input and print squared 'n' array elements

---
+++ ex8_3_1/ex8_3_1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Input number of array elements:");
+            int numberOfUserInputs = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[numberOfUserInputs];
+            int i;
+
+            Console.WriteLine("Input " + numberOfUserInputs + " elements in the array :");
+            for (i = 0; i < numberOfUserInputs; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+
+            Console.WriteLine("Squared elements in array are: ");
+            for (i = 0; i < numberOfUserInputs; i++)
+            {
+                int Sqrt = array[i] * array[i];
+                System.Console.WriteLine("Sqrt element - " + i + " : " + Sqrt);
+
+            }
+
+        }
+    }
+}
