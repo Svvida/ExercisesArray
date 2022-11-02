ca436cd157d6bbdfaf8cf94224322df905f3e983
Swida <ksvvida@gmail.com>
2022-11-02 10:36:56 +0100

    Read 'n' array elements then sort, multiply by 2 and print them

---
+++ ex8_3_4/ex8_3_4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Input number of array elements:");
+            int numberOfUserInputs = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[numberOfUserInputs];
+            int i;
+
+            Console.WriteLine("Input " + numberOfUserInputs + " elements in the array :");
+            for (i = 0; i < numberOfUserInputs; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            System.Console.WriteLine("Sorted array:");
+            Array.Sort(array);
+            foreach (int k in array)
+            {
+                System.Console.WriteLine(k);
+            }
+
+            Console.WriteLine("Multiplied by 2 elements in array: ");
+            for (i = 0; i < numberOfUserInputs; i++)
+            {
+                int multiply = array[i] * 2;
+                System.Console.WriteLine("Multiplied element - " + i + " : " + multiply);
+
+            }
+
+        }
+    }
+}
