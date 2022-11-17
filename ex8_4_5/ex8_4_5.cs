2cd212b31a5db82cf6d991481791a34857cbe101
Swida <ksvvida@gmail.com>
2022-11-17 19:46:33 +0100

    Find numbers with all even digits

---
+++ ex8_4_5/ex8_4_5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[] { "2", "4", "5", "10", "22", "32", "213", "222" };
+            for (int arrayElement = 0; arrayElement < array.Length; arrayElement++)
+            {
+                bool isAllDigitsEven = true;
+                string number = array[arrayElement]; // array[i]
+                int numberOfDigits = number.Length;
+                for (int digit = 0; digit < numberOfDigits; digit++)
+                {
+                    if (number[digit] % 2 != 0) isAllDigitsEven = false;
+                }
+                if (isAllDigitsEven == true)
+                {
+                    System.Console.WriteLine(number);
+                }
+            }
+
+        }
+    }
+}
