cc3cbf20f25e6bae6d71209ebab48c98a690ce65
Swida <ksvvida@gmail.com>
2022-11-24 18:39:58 +0100

    Find numbers that sum of digits is equal to one

---
+++ ex8_4_6/ex8_4_6.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[] { "1", "2", "4", "5", "7", "10", "22", "32", "213", "222", "100", "1000" };
+            for (int arrayElement = 0; arrayElement < array.Length; arrayElement++)
+            {
+                bool isSumOfDigitsEqualOne = true;
+                int NumberOfDigits = array[arrayElement].Length;
+
+                for (int digit = 0; digit < NumberOfDigits; digit++)
+                {
+                    int sum = 0;
+                    int ElementTablicy = Convert.ToInt32(array[arrayElement]);
+                    while (ElementTablicy != 0)
+                    {
+                        sum += ElementTablicy % 10;
+                        ElementTablicy /= 10;
+                    }
+
+                    if (sum > 1)
+                    {
+                        isSumOfDigitsEqualOne = false;
+                    }
+                }
+
+
+                if (isSumOfDigitsEqualOne == true)
+                {
+                    System.Console.WriteLine(array[arrayElement]);
+                }
+
+            }
+
+        }
+    }
+}
