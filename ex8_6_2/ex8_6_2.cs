8e942e6d0808c8066ede025182195bb174331158
Swida <ksvvida@gmail.com>
2022-12-01 21:58:44 +0100

    Print 'n' first Fibonacci sequence numbers

---
+++ ex8_6_2/ex8_6_2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace exercise
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.Write("Enter how many Fibonacci sequence numbers you want to be written(two first numbers will be printed no mater of what): ");
+            int numberOfElements = Convert.ToInt16(Console.ReadLine());
+
+            System.Console.WriteLine(0);
+            System.Console.WriteLine(1);
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                int c = a + b;
+                System.Console.WriteLine(c);
+                a = b;
+                b = c;
+            }
+
+        }
+    }
+}
