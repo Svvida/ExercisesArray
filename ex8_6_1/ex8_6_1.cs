ddaf82e9629485dc4a8d77372513c6a62367a90a
Swida <ksvvida@gmail.com>
2022-12-01 18:36:55 +0100

    Read 'n' list elements and print only primary numbers

---
+++ ex8_6_1/ex8_6_1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace exercise
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("Input number of List elements: ");
+            int numberOfListElements = Convert.ToInt16(Console.ReadLine());
+            List<int> list = new List<int>(numberOfListElements);
+            int i;
+            for (i = 2; i < numberOfListElements; i++)
+            {
+                list.Add(i);
+            }
+
+
+            int squareOfNumberOfListElements = Convert.ToInt16(Math.Sqrt(numberOfListElements));
+            int k;
+            for (k = 2; k < squareOfNumberOfListElements + 1; k++)
+            {
+                for (i = 2; i < list.Count; i++)
+                {
+                    if (list[i] % k == 0 && list[i] > k)
+                    {
+                        list.Remove(list[i]);
+                    }
+                }
+            }
+
+
+            System.Console.WriteLine("Primary numbers: ");
+            for (int w = 0; w < list.Count; w++)
+            {
+                System.Console.WriteLine(list[w]);
+            }
+        }
+    }
+}
