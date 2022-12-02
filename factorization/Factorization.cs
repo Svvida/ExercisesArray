75fda74bc69005ce9375ea609c6695b55814072d
Swida <ksvvida@gmail.com>
2022-12-02 15:05:06 +0100

    Factorization

---
+++ factorization/Factorization.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections;
+
+namespace exercise
+{
+    internal class Program
+    {
+
+
+        static ArrayList naCzynniki(double liczba)
+        {
+            ArrayList lista = new ArrayList();
+            int k = 2;
+            while (liczba != 1)
+            {
+                while (liczba % k == 0)
+                {
+                    lista.Add(k);
+                    liczba = liczba / k;
+                }
+                k++;
+            }
+            return lista;
+        }
+
+
+        static void Main(string[] args)
+        {
+            System.Console.Write("Pleace enter number you want to factorize: ");
+            int yourNumber = int.Parse(System.Console.ReadLine());
+
+            ArrayList czynniki = naCzynniki(yourNumber);
+            foreach (var czynnik in czynniki)
+            {
+                Console.WriteLine(czynnik);
+            }
+
+        }
+    }
+}
