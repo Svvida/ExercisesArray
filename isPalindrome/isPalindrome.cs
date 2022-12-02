027c658974f0de6abb27306584f38b387ed443a0
Swida <ksvvida@gmail.com>
2022-12-02 11:45:38 +0100

    Checking if a word is a palindrome

---
+++ isPalindrome/isPalindrome.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace exercise
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.Write("Enter a word you want to check- ");
+            string word = Console.ReadLine();
+            System.Console.WriteLine("Is " + word + " a palindrome? - : " + isPalindrome(word));
+        }
+
+        static bool isPalindrome(String word)
+        {
+            if (word.Length == 0) return false;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] != word[word.Length - 1 - i]) return false;
+
+            }
+            return true;
+        }
+    }
+}
