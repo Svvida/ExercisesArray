fa1f32079f48330aaf9f7735ac91d8f042a8a20b
Swida <ksvvida@gmail.com>
2022-12-07 16:08:35 +0100

    Filling the array in different ways

---
+++ ex8_6_4/ex8_6_4.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+namespace exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Input number of array elements:");
+            int numberOfUserInputs = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[numberOfUserInputs];
+            int i;
+
+            //ex8_6_4 A
+            // Console.WriteLine("Input " + numberOfUserInputs + " elements in the array :");
+            // for (i = 0; i < numberOfUserInputs; i++)
+            // {
+            //     array[i] = i;
+            //     Console.WriteLine("element - {0} : {1}", i, i);
+
+            // }
+
+            //ex8_6_4 B
+            // int el = 7;
+            // Console.WriteLine("Input " + numberOfUserInputs + " elements in the array :");
+            // for (i = 0; i < numberOfUserInputs; i++)
+            // {
+            //     array[i] = el;
+            //     Console.WriteLine("element - {0} : {1}", i, el);
+            //     el += 1;
+
+            // }
+
+            //ex8_6_4 C
+            // int el = 4;
+            // Console.WriteLine("Input " + numberOfUserInputs + " elements in the array :");
+            // for (i = 0; i < numberOfUserInputs; i++)
+            // {
+            //     array[i] = el;
+            //     Console.WriteLine("element - {0} : {1}", i, el);
+            //     el += 4;
+
+            // }
+
+            //ex8_6_4 D
+            // int el = 1;
+            // Console.WriteLine("Input " + numberOfUserInputs + " elements in the array :");
+            // for (i = 0; i < numberOfUserInputs; i++)
+            // {
+            //     array[i] = el;
+            //     Console.WriteLine("element - {0} : {1}", i, el);
+            //     el *= 2;
+
+            // }
+
+            //ex8_6_4 E
+            int el = 2;
+            Console.WriteLine("Input " + numberOfUserInputs + " elements in the array :");
+            for (i = 0; i < numberOfUserInputs; i++)
+            {
+                array[i] = el;
+                Console.WriteLine("element - {0} : {1}", i, el);
+                el += 1;
+
+            }
+        }
+    }
+}
